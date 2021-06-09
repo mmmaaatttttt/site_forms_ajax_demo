f8c2f21d975c2e1419508d577a093dbed553edcb
Matt Lane <lane.matthew@gmail.com>
2021-06-09 09:52:36 -0700

    successfully make API call and log data

--- app.js
+++ app.js
@@ -5,12 +5,17 @@ const pokemonForm = document.querySelector("form");
 // 1. control form submit behavior with JS
 pokemonForm.addEventListener("submit", getPokemon);
 
-function getPokemon(event) {
+async function getPokemon(event) {
   event.preventDefault();
   const pokemonInput = event.target.pokemon;
   const pokemon = pokemonInput.value;
-  console.log("pokemon is", pokemon);
+  const apiUrl = "https://pokeapi.co/api/v2/pokemon/" + pokemon;
+  console.log("apiUrl is", apiUrl);
+  
   // 2. On form submit, go to the pokemon API 
+  const response = await fetch(apiUrl);
+  const responseData = await response.json();
+  console.log("response stuff", response, responseData);
   // 3. Get data on the pokemon and
   //   put the image of the pokemon on the page
 }
