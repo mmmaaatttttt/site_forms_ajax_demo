36da91d0203b04076c49c447c57194c94b8233c6
Matt Lane <lane.matthew@gmail.com>
2021-06-09 10:05:37 -0700

    show pokemon data in the DOM after fetching

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 console.log("page loaded");
 
 const pokemonForm = document.querySelector("form");
+const pokemonArea = document.querySelector("#pokemon-area");
 
 // 1. control form submit behavior with JS
 pokemonForm.addEventListener("submit", getPokemon);
@@ -10,12 +11,21 @@ async function getPokemon(event) {
   const pokemonInput = event.target.pokemon;
   const pokemon = pokemonInput.value;
   const apiUrl = "https://pokeapi.co/api/v2/pokemon/" + pokemon;
-  console.log("apiUrl is", apiUrl);
   
   // 2. On form submit, go to the pokemon API 
   const response = await fetch(apiUrl);
   const responseData = await response.json();
-  console.log("response stuff", response, responseData);
+
   // 3. Get data on the pokemon and
   //   put the image of the pokemon on the page
+  generateHTML(responseData);
 }
+
+function generateHTML(pokeData) {
+  pokemonArea.innerHTML = `
+    <h1>${pokeData.name}</h1>
+    <p>Height: ${pokeData.height}</p>
+    <p>Weight: ${pokeData.weight}</p>
+    <img src="${pokeData.sprites.front_default}" alt="${pokeData.name} front default" />
+  `;
+} 
